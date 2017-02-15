ccbf5861100d7f70987849ab11fb389a96967de0
yangzc <yangzc@knowbox.cn>
2017-02-15 15:51:27 +0800

    bugfix

--- samples/src/main/java/com/hyena/coretext/samples/question/ImageBlock.java
+++ samples/src/main/java/com/hyena/coretext/samples/question/ImageBlock.java
@@ -5,11 +5,9 @@
 package com.hyena.coretext.samples.question;
 
 import android.content.Context;
-import android.graphics.Bitmap;
 
 import com.hyena.coretext.TextEnv;
 import com.hyena.coretext.blocks.CYImageBlock;
-import com.hyena.coretext.samples.R;
 import com.hyena.framework.clientlog.LogUtil;
 import com.hyena.framework.utils.UIUtils;
 
@@ -21,15 +19,10 @@ import org.json.JSONObject;
  */
 public class ImageBlock extends CYImageBlock {
 
-    private static float MAX_HEIGHT = 0;
-    private boolean mIsBigImage = false;
     private float mScreenWidth = 0;
-
-    private int mWidth, mHeight;
 
     public ImageBlock(TextEnv textEnv, String content) {
         super(textEnv, content);
-        MAX_HEIGHT = UIUtils.dip2px(100);
         mScreenWidth = textEnv.getContext().getResources().getDisplayMetrics().widthPixels;
         init(textEnv.getContext(), content);
     }
@@ -42,34 +35,19 @@ public class ImageBlock extends CYImageBlock {
             LogUtil.v("yangzc", content);
             if ("big_image".equals(size)) {
                 setAlignStyle(AlignStyle.Style_MONOPOLY);
-                mIsBigImage = true;
                 setWidth((int) mScreenWidth);
-                setHeight(100);
+                setHeight((int) (mScreenWidth / 2));
             } else if ("small_img".equals(size)) {
-                mIsBigImage = false;
                 setWidth(UIUtils.dip2px(37));
                 setHeight(UIUtils.dip2px(37));
             } else {
-                mIsBigImage = false;
                 setWidth(UIUtils.dip2px(60));
                 setHeight(UIUtils.dip2px(60));
             }
-            setResUrl(context, url, R.drawable.baidu);
+            setResUrl(url);
         } catch (JSONException e) {
             e.printStackTrace();
         }
-    }
-
-    @Override
-    protected void setBitmap(Bitmap bitmap) {
-        super.setBitmap(bitmap);
-        if (mIsBigImage && mBitmap != null && !mBitmap.isRecycled()) {
-            float scale = Math.min(mScreenWidth/mBitmap.getWidth(), MAX_HEIGHT / mBitmap.getHeight());
-            setWidth((int) (mBitmap.getWidth() * scale));
-            setHeight((int) (mBitmap.getHeight() * scale));
-            getTextEnv().getEventDispatcher().requestLayout();
-        }
-        getTextEnv().getEventDispatcher().postInvalidate();
     }
 
 }
