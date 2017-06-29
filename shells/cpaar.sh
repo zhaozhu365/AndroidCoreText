be6565e9ad00cf248deaae3ed98f6e269a311a80
yangzc <yangzc@knowbox.cn>
2017-06-29 20:34:29 +0800

    modify build.gradle

--- shells/cpaar.sh
+++ shells/cpaar.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
+./gradlew library:assembleRelease
 cp ~/devsoft/projects/android_st/AndroidCoreText/library/build/outputs/aar/library-release.aar /Users/yangzc/devsoft/projects/android/AndroidKnowboxBase/coretext/coretext.aar
